97fdba0984a8950bc8c6283cff0da361d727612d
AlixVP <alix.vinotprefontaine@gmail.com>
2018-04-10 11:32:11 +0200

    cours 4 - partie 1

---
+++ cours4/script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+  function welcome() {
+   $("#welcome").html('<h4>Bienvenue, <br> soyez prêt à renouveler de nouveaux défis !</h4>');
+};
+//Appel de la fonction
+  welcome();
+})
